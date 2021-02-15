c5c4c675af8a502f6c1ccbe44f8a2e65cc6c0e4d
Zakariya Qureshi <zakariyaq313@gmail.com>
2021-02-15 23:25:08 +0530

    Major update

--- main.js
+++
@@ -1,7 +0,0 @@
-let likeButtons = document.querySelectorAll("button.like-button");
-
-for (let i = 0; i < likeButtons.length; i++) {
-    likeButtons[i].addEventListener("click", () => {
-        likeButtons[i].classList.toggle("favourite");
-    })
-}
